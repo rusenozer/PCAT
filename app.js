57bef0d0feffd8ea937a64039a7c3217a4b65151
rusoz <rusenozer85@gmail.com>
2022-03-06 14:29:14 +0300

    uploading files
    static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const fn = function () {
-  console.log('functiyon worknig');
-};
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//MiddleWares
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı`);
+});
